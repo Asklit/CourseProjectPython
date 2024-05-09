6db69a3a8657e65027889973c656e7c0d9d12344
Alexandr Khanzhin <forgrace5@gmail.com>
2024-05-09 12:06:28 +0500

    A: Добавил возможность выбора нескольких файлов

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.GoToSettings = None
         self.fileFlag = False
+        self.fileNames = []
         uic.loadUi('uiMainFile.ui', self)  # Открытие файла ui
 
         self.LEErrorLine.hide()
@@ -21,10 +22,12 @@ class MainWindow(QMainWindow):
         self.btnSettings.clicked.connect(self.setupSettings)    # Кнопка перехода к настройкам
 
     def chooseFile(self):  # Выбор файла
-        dialog = QFileDialog.getOpenFileName(self, "Выбор файла", "", "*.docx")
+        dialog = QFileDialog.getOpenFileNames(self, "Выбор файла", "", "*.docx")
         if dialog[0]:
+            print(dialog[0])
             self.fileFlag = True
-            self.LENameFile.setText(dialog[0])
+            self.LENameFile.setText(f"Выбрано файлов: {len(dialog[0])} ")
+            self.fileNames = dialog[0]
             self.LEErrorLine.hide()
 
     def runCheckingCorrectness(self):  # Запуск проверки корректности
