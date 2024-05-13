d51addc70a31d06adbaa14fd8c17a66767e830e1
Alexandr Khanzhin <forgrace5@gmail.com>
2024-05-13 14:08:04 +0500

    A: Подключение справки

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ class MainWindow(QMainWindow):
         self.table_checklist = None
         self.picture_checklist = None
 
+        self.proc = None
+
         keyboard.add_hotkey("f1", self.openDocumentation)
 
         self.LEErrorLine.hide()
@@ -37,10 +39,13 @@ class MainWindow(QMainWindow):
         self.btnSettings.clicked.connect(self.setupSettings)    # Кнопка перехода к настройкам
 
     def openDocumentation(self):
+        if self.proc is not None:
+            self.proc.kill()
+
         if self.isActiveWindow():
-            self.proc = subprocess.Popen("hh.exe -mapid" + "00" "HelpMenu.chm")
+            self.proc = subprocess.Popen("hh.exe -mapid" + "100" + " HelpMenu.chm")
         else:
-            self.proc = subprocess.Popen("hh.exe -mapid" + "1" + str(self.SettingsWindow.tabWidget.currentIndex()) + "HelpMenu.chm")
+            self.proc = subprocess.Popen("hh.exe -mapid" + "20" + str(self.SettingsWindow.tabWidget.currentIndex() + 1) + " HelpMenu.chm")
 
     def chooseFile(self):  # Выбор файла
         dialog = QFileDialog.getOpenFileNames(self, "Выбор файла", "", "*.docx")
