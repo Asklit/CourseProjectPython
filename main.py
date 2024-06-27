7b4a44784a79623772ba64a31877227f6e3ad6b8
Xenx3127 <slava@DESKTOP-3RT9RNJ>
2024-06-27 20:02:27 +0500

    bugfix

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ class Worker(QThread):
         self.parser = parser
 
     def doc_finished(self, res):
-
         self.doc_progressed.emit()
 
     def run(self):
@@ -399,8 +398,8 @@ class MainWindow(QMainWindow):
             "font_bald": self.SettingsWindow.ui.CBMainTextBold.isChecked(),
             "font_italic": self.SettingsWindow.ui.CBMainTextItalic.isChecked(),
             "font_underline": self.SettingsWindow.ui.CBMainTextUnderline.isChecked(),
-            "font_color": None,  # нет такого в UI
-            "font_back_color": None,  # нет такого в UI
+            "font_color": False,  # нет такого в UI
+            "font_back_color": False,  # нет такого в UI
             "alignment": alignment,
             "keep_with_next": False,  # нет такого в UI, false по умолчанию для main текста
             "page_break_before": False,  # нет такого в UI, false по умолчанию для main текста
@@ -429,8 +428,8 @@ class MainWindow(QMainWindow):
             "font_bald": self.SettingsWindow.ui.CBLVL1Bold.isChecked(),
             "font_italic": self.SettingsWindow.ui.CBLVL1Italic.isChecked(),
             "font_underline": self.SettingsWindow.ui.CBLVL1Underline.isChecked(),
-            "font_color": None,  # нет такого в UI
-            "font_back_color": None,  # нет такого в UI
+            "font_color": False,  # нет такого в UI
+            "font_back_color": False,  # нет такого в UI
             "alignment": alignment,
             "keep_with_next": self.SettingsWindow.ui.CBLVL1NotSpacing.isChecked(),
             "page_break_before": self.SettingsWindow.ui.CBLVL1NewPage.isChecked(),  # нет такого в UI, false по умолчанию для main текста
@@ -460,8 +459,8 @@ class MainWindow(QMainWindow):
             "font_bald": self.SettingsWindow.ui.CBLVL2Bold.isChecked(),
             "font_italic": self.SettingsWindow.ui.CBLVL2Italic.isChecked(),
             "font_underline": self.SettingsWindow.ui.CBLVL2Underline.isChecked(),
-            "font_color": None,  # нет такого в UI
-            "font_back_color": None,  # нет такого в UI
+            "font_color": False,  # нет такого в UI
+            "font_back_color": False,  # нет такого в UI
             "alignment": alignment,
             "keep_with_next": self.SettingsWindow.ui.CBLVL2NotSpacing.isChecked(),
             "page_break_before": self.SettingsWindow.ui.CBLVL2NewPage.isChecked(),  # нет такого в UI, false по умолчанию для main текста
@@ -491,8 +490,8 @@ class MainWindow(QMainWindow):
             "font_bald": self.SettingsWindow.ui.CBLVL3Bold.isChecked(),
             "font_italic": self.SettingsWindow.ui.CBLVL3Italic.isChecked(),
             "font_underline": self.SettingsWindow.ui.CBLVL3Underline.isChecked(),
-            "font_color": None,  # нет такого в UI
-            "font_back_color": None,  # нет такого в UI
+            "font_color": False,  # нет такого в UI
+            "font_back_color": False,  # нет такого в UI
             "alignment": alignment,
             "keep_with_next": self.SettingsWindow.ui.CBLVL3NotSpacing.isChecked(),
             "page_break_before": self.SettingsWindow.ui.CBLVL3NewPage.isChecked(),  # нет такого в UI, false по умолчанию для main текста
